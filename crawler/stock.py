5caa25db7a8350e8edaf74bc70e8eedd86c35a4d
will <wp20131020ad@gmail.com>
2023-07-01 12:48:37 +0800

    '新增例外處理'

--- crawler/stock.py
+++ crawler/stock.py
@@ -4,20 +4,21 @@ from bs4 import BeautifulSoup
 
 def get_stock():
     url='https://tw.stock.yahoo.com/'
-
-    resp=requests.get(url)
-    soup=BeautifulSoup(resp.text,'lxml')
-    lis=soup.find('nav',class_="indexChartNav").find_all('li')
-
     stocks=[]
+    try:
+        resp=requests.get(url)
+        soup=BeautifulSoup(resp.text,'lxml')
+        lis=soup.find('nav',class_="indexChartNav").find_all('li')
 
-    for li in lis:
-        spans=li.find_all('span')[:2]
-        data={'分類':spans[0].text.strip(),
-            '指數':spans[1].text.strip()}
-        stocks.append(data)
-        
-    return(stocks)
+
+        for li in lis:
+            spans=li.find_all('span')[:2]
+            data={'分類':spans[0].text.strip(),'指數':spans[1].text.strip()}
+            stocks.append(data)
+            
+    except Exception as e:
+        print(e)
+    return stocks
 
 if __name__ == "__main__":
     print(get_stock())
